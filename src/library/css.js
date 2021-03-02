e3456b735cb2db9886d8210e3194617d531130f7
David Bushell <david@dbushell.com>
2021-03-02 16:36:18 +0000

    sass importer

--- src/library/css.js
+++ src/library/css.js
@@ -3,14 +3,18 @@ import path from 'path';
 import crypto from 'crypto';
 import csso from 'csso';
 import sass from 'node-sass';
-import magicImporter from 'node-sass-magic-importer';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 let css = fs.readFileSync(path.resolve(__dirname, `../scss/main.scss`));
+
+const components = fs.readdirSync(path.resolve(__dirname, `../scss/components`));
+components.forEach((file) => {
+  css += `@import 'src/scss/components/${file}';\n`;
+});
+
 css = sass.renderSync({
-  data: css.toString(),
-  importer: magicImporter()
+  data: css.toString()
 }).css;
 css = csso.minify(css.toString()).css;
 const cssHash = crypto.createHash('sha256').update(css).digest('base64');
