c74a2a129ae52b841c7001805a56051c3f6843eb
David Bushell <david@dbushell.com>
2021-03-11 17:37:34 +0000

    netlify deno test (5)

--- src/deno/netlify.sh
+++ src/deno/netlify.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+HOME_BIN=$(pwd)/bin/x86_64-unknown-linux-gnu
+mkdir -p $HOME_BIN
+
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
-# curl -fsSL https://github.com/sass/dart-sass/releases/download/1.32.8/dart-sass-1.32.8-linux-x64.tar.gz \
-#   | tar -zx --strip-components 1 dart-sass/sass
+curl -fsSL https://github.com/sass/dart-sass/releases/download/1.32.8/dart-sass-1.32.8-linux-x64.tar.gz \
+  | tar -zx --strip-components 1 dart-sass/sass
 
-echo $(pwd)
+mv -f /opt/buildhome/.deno/bin/deno $HOME_BIN
+mv -f sass $HOME_BIN
 
-ls -la ./
+export PATH="$HOME_BIN:$PATH"
 
-/opt/buildhome/.deno/bin/deno info
+ls -la $HOME_BIN
+
+deno --version
+sass --verison
