7d32f6a472b5a480f70498dd3523545269f912e2
David Bushell <david@dbushell.com>
2021-07-08 08:33:09 +0100

    imports

--- src/init.sh
+++ src/init.sh
@@ -6,6 +6,6 @@ deno --version
 source src/build.sh
 
 deno install --force --allow-net --allow-read \
-  https://deno.land/std@0.99.0/http/file_server.ts
+  https://deno.land/std@0.100.0/http/file_server.ts
 
 file_server -p 8080 ./public/
